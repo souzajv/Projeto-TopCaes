87dbdf42292756abffaaffe5fc10c298e66b477d
João Souza <51976773+souzajv@users.noreply.github.com>
2024-09-28 03:25:00 -0300

    fix: i removido da função do componente carousel.tsx

---
+++ src/app/components/carousel.tsx
@@ -0,0 +1,79 @@
+"use client";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import CarouselCard from './carouselCard';
+
+function Carousel() {
+    const settings = {
+        dots: true,
+        arrows: false,
+        infinite: true,
+        speed: 500,
+        autoplay: true,
+        autoplaySpeed: 5500,
+        cssEase: "linear",
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        initialSlide: 0,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    initialSlide: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ],
+        customPaging: () => (
+            <div></div>
+        )
+    };
+
+    return (
+        <div className="slider-container w-full">
+            <Slider {...settings}>
+                <div className="!flex !justify-center">
+                    <CarouselCard
+                        image='/assets/Cliente1.png'
+                        alt='Imagem do cachorro de um cliente do Rodrigo'
+                        text='“Lorem ipsum dolor sit amet, consectetur adipiscing elit. “'
+                    />
+                </div>
+                <div className="!flex !justify-center">
+                    <CarouselCard
+                        image='/assets/Cliente2.png'
+                        alt='Imagem do cachorro de um cliente do Rodrigo'
+                        text='“Lorem ipsum dolor sit amet, consectetur adipiscing elit. “'
+                    />
+                </div>
+                <div className="!flex !justify-center">
+                    <CarouselCard
+                        image='/assets/Cliente3.png'
+                        alt='Imagem do cachorro de um cliente do Rodrigo'
+                        text='“Lorem ipsum dolor sit amet, consectetur adipiscing elit. “'
+                    />
+                </div>
+            </Slider>
+        </div>
+    );
+}
+
+export default Carousel;
